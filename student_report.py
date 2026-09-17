a021c8d73735e055b1971e1d1bb8f753e663a08a
kanish0011 <k93730057@gmail.com>
2026-09-17 08:39:28 +0530

    Create student_report.py to generate academic report

---
+++ student_report.py
@@ -0,0 +1,4 @@
+with open("academic_report.txt", "w") as f:
+    f.write("=== Student Academic Performance Report ===\n")
+    f.write("Total Students Tracked: 500\n")
+print("Report file generated.")
